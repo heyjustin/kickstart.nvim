1359992f20fb59b066da59205c8b1258b88ff15b
Justin Walsh <juswalsh@amazon.com>
2025-05-30 10:20:16 +0200

    Use C-\ to toggle terminal visibility

--- lua/custom/plugins/toggle-term.lua
+++ lua/custom/plugins/toggle-term.lua
@@ -2,4 +2,9 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {},
+  config = function()
+    require('toggleterm').setup {
+      open_mapping = [[<c-\>]], -- Key to toggle terminal visibility
+    }
+  end,
 }
